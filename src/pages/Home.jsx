7ae1c32727022b27266a202ad41abb332a814006
Ingelinn Helene <ingelinn92@gmail.com>
2025-01-30 10:57:23 +0100

    create project structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { Heading, Text, Button, VStack } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <VStack spacing={4} textAlign='center'>
+      <Heading>Welcome to AuctionWave!</Heading>
+      <Text>Sign up today and get 1000 free credits to start bidding!</Text>
+      <Button as={Link} to='/auctions' colorScheme='blue'>
+        View Auctions
+      </Button>
+    </VStack>
+  );
+}
+
+export default Home;
