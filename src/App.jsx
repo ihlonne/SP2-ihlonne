46ae37ae3a98f0829ce2ea65da1e0d0f455baa17
Ingelinn Helene <ingelinn92@gmail.com>
2025-01-31 09:27:44 +0100

    add react router to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { useMediaQuery } from '@chakra-ui/react';
 
 import Header from './components/Layout/Header/Header';
@@ -5,13 +7,24 @@ import Navbar from './components/Layout/Header/Navbar';
 import MobileHeader from './components/Layout/Header/MobileHeader';
 import MobileNavbar from './components/Layout/Header/MobileNavbar';
 
+import Home from './pages/Home';
+import Auctions from './pages/Auctions';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
+
 function App() {
   const [isLargerThan900] = useMediaQuery('(min-width: 900px)');
   return (
-    <>
+    <Router>
       {isLargerThan900 ? <Header /> : <MobileHeader />}
       {isLargerThan900 ? <Navbar /> : <MobileNavbar />}
-    </>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/auctions' element={<Auctions />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
