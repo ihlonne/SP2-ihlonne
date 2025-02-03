d876be123d7c2e0ea9b907b5cf43fda24b189350
Ingelinn Helene <ingelinn92@gmail.com>
2025-02-03 15:39:37 +0100

    remove unnecessary if statement

--- src/hooks/authUtils.js
+++ src/hooks/authUtils.js
@@ -15,10 +15,15 @@ export async function handleRegister(userData) {
 export async function handleLogin(credentials) {
   try {
     const response = await loginUser(credentials);
+
+    localStorage.setItem('token', response.data.accessToken);
+    localStorage.setItem('user', JSON.stringify(response.data));
+
     console.log('Login successful:', response);
+    console.log('AccessToken:', response.data.accessToken);
     return response;
   } catch (error) {
-    console.error('Error logging inn:', error);
+    console.error('Error logging inn:', error.response?.data || error);
     throw error;
   }
 }
