f0d546103733c3973dbcf468517a48caa98f2d32
Ingelinn Helene <ingelinn92@gmail.com>
2025-02-04 16:34:44 +0100

    update mobile navbar

--- src/components/Layout/Header/MobileNavbar.jsx
+++ src/components/Layout/Header/MobileNavbar.jsx
@@ -1,4 +1,5 @@
 import {
+  Avatar,
   Image,
   Text,
   Icon,
@@ -22,10 +23,16 @@ import {
 import { HamburgerIcon } from '@chakra-ui/icons';
 import { FaSignInAlt } from 'react-icons/fa';
 import { TiUserAdd } from 'react-icons/ti';
+
+import { FiLogOut } from 'react-icons/fi';
 import logo from '../../../assets/sp2logodark.png';
 import CustomModal from '../../UI/Modal';
+import { useAuth } from '../../../hooks/useAuth';
+import { handleLogout } from '../../../hooks/authUtils';
 
 function MobileNavbar() {
+  const { user, setUser } = useAuth();
+
   // Manage drawer state
   const {
     isOpen: isMenuOpen,
@@ -67,26 +74,71 @@ function MobileNavbar() {
       bg='brand.100'
       w='full'
     >
-      <Flex flexDirection='column' justify='center'>
-        <IconButton bg='brand.100' onClick={openMenu}>
-          <HamburgerIcon boxSize='8' />
-        </IconButton>
-        <Text fontSize='xs'>Categories</Text>
-      </Flex>
+      {user?.accessToken ? (
+        <Flex
+          position='fixed'
+          bottom='0'
+          left='0'
+          justify='space-between'
+          align='center'
+          p='15px'
+          bg='brand.100'
+          w='full'
+        >
+          <Flex flexDirection='column' justify='center'>
+            <IconButton bg='brand.100' onClick={openMenu}>
+              <HamburgerIcon boxSize='8' />
+            </IconButton>
+            <Text fontSize='xs'>Categories</Text>
+          </Flex>
+          <Flex gap='4' align='center'>
+            <Text>1000 credits</Text>
+            <Avatar size='sm' name={user?.name} src={user?.avatar?.url} />
 
-      <Flex flexDirection='column' justify='center'>
-        <IconButton bg='brand.100' onClick={openLogin}>
-          <Icon as={FaSignInAlt} boxSize='8' />
-        </IconButton>
-        <Text fontSize='xs'>Sign in</Text>
-      </Flex>
+            <IconButton
+              bg='transparent'
+              color='black'
+              onClick={() => {
+                handleLogout(setUser);
+              }} // ✅ Call function on click
+            >
+              <Icon as={FiLogOut} boxSize='8' />
+            </IconButton>
+          </Flex>
+        </Flex>
+      ) : (
+        <Flex
+          position='fixed'
+          bottom='0'
+          left='0'
+          justify='space-between'
+          align='center'
+          p='15px'
+          bg='brand.100'
+          w='full'
+        >
+          <Flex flexDirection='column' justify='center'>
+            <IconButton bg='brand.100' onClick={openMenu}>
+              <HamburgerIcon boxSize='8' />
+            </IconButton>
+            <Text fontSize='xs'>Categories</Text>
+          </Flex>
 
-      <Flex flexDirection='column' justify='center'>
-        <IconButton bg='brand.100' onClick={openRegister}>
-          <Icon as={TiUserAdd} boxSize='8' color='brand.600' />
-        </IconButton>
-        <Text fontSize='xs'>Register</Text>
-      </Flex>
+          <Flex flexDirection='column' justify='center'>
+            <IconButton bg='brand.100' onClick={openLogin}>
+              <Icon as={FaSignInAlt} boxSize='8' />
+            </IconButton>
+            <Text fontSize='xs'>Sign in</Text>
+          </Flex>
+
+          <Flex flexDirection='column' justify='center'>
+            <IconButton bg='brand.100' onClick={openRegister}>
+              <Icon as={TiUserAdd} boxSize='8' color='brand.600' />
+            </IconButton>
+            <Text fontSize='xs'>Register</Text>
+          </Flex>
+        </Flex>
+      )}
 
       <Drawer placement='left' onClose={closeMenu} isOpen={isMenuOpen}>
         <DrawerOverlay />
